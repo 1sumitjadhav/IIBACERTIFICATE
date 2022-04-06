97898352ece3347f1420b9a32adeb4db3a629250
rathod <rathodpraveen440@gmail.com>
2022-04-06 20:00:28 +0530

    New Code for coupons

--- Coupon Code Generater/Coupon_Code_Generator/Coupon_Code_Generator/wsgi.py
+++ Coupon Code Generater/Coupon_Code_Generator/Coupon_Code_Generator/wsgi.py
@@ -1,16 +1,16 @@
 """
-WSGI config for Coupon_Generator project.
+WSGI config for Coupon_Code_Generator project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Coupon_Generator.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Coupon_Code_Generator.settings')
 
 application = get_wsgi_application()
--- Coupon Code Generater/Coupon_Code_Generator/manage.py
+++ Coupon Code Generater/Coupon_Code_Generator/manage.py
@@ -5,7 +5,8 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Coupon_Generator.settings')
+    """Run administrative tasks."""
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Coupon_Code_Generator.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
