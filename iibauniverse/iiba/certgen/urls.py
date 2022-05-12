1c68f55e8ebc386a5e4b29798d21d6e9e94580b4
MeJaM35 <jamsutkarmeetpradeep@gmail.com>
2022-05-12 19:50:48 +0530

    rebase

--- iibauniverse/iiba/certgen/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import Upload
-
-urlpatterns = [
-    path('upload', Upload, name='upload' ),
-
-]
--- iibauniverse/iiba/coupgen/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from django.urls import include, path
-from . import views
-
-urlpatterns = [
-   path('couponVal',views.couponVal, name='couponVal'),
-   path('generatecoupon',views.generatecoupon, name='generate'),
-   path('<int:id>/checkingCouponStatus',views.checkingCouponStatus,name='checkingCouponStatus'),  ]
